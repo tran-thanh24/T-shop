50d47d229bd9463e000d6718ac9061b9d40ea87f
tran-thanh24 <thanheo203@gmail.com>
2023-10-24 17:32:34 +0700

    commit

--- JS/login.js
+++ JS/login.js
@@ -1,9 +1,18 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
+const dkyBtn = document.getElementById('dky');
 const loginBtn = document.getElementById('login');
 const backBtn = document.getElementById("back");
+const phone = document.getElementById('phone');
+const email = document.getElementById('email');
 
 registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+dkyBtn.addEventListener('click', () => {
+    validatePhone(phone);
+    validateEmail(email);
     container.classList.add("active");
 });
 
@@ -12,21 +21,20 @@ loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
 
-backBtn.addEventListener('click', () => {
-    window.location.href = "";
-});
+// backBtn.addEventListener('click', () => {
+//     window.location.href = "";
+// });
 
 
 // kiểm tra email, sđt
 // validation.js
 
-function validatePhone(phoneInput, phoneError) {
+function validatePhone(phoneInput) {
     const phoneNumber = phoneInput.value;
     if (!/^\d{10,11}$/.test(phoneNumber)) {
-        phoneError.textContent = "Phone number must be 10-11 digits.";
+        alert('Phone number must be 10-11 digits.')
         return false;
     } else {
-        phoneError.textContent = "";
         return true;
     }
 }
@@ -41,4 +49,3 @@ function validateEmail(emailInput) {
         return false;
     }
 }
-
